cb297efc14dd7b2cfdc48d1756560b9592df68b3
Sanna Balouchi <105402020+saunade@users.noreply.github.com>
2024-05-06 13:59:06 -0400

    added debug folder

---
+++ tester_rs_simple/debug/build/libc-9b180bf7b5b2e059/build_script_build-9b180bf7b5b2e059.d
@@ -0,0 +1,5 @@
+/Users/sannabalouchi/Downloads/tester_rs_simple/target/debug/build/libc-9b180bf7b5b2e059/build_script_build-9b180bf7b5b2e059: /Users/sannabalouchi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.153/build.rs
+
+/Users/sannabalouchi/Downloads/tester_rs_simple/target/debug/build/libc-9b180bf7b5b2e059/build_script_build-9b180bf7b5b2e059.d: /Users/sannabalouchi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.153/build.rs
+
+/Users/sannabalouchi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.153/build.rs:
